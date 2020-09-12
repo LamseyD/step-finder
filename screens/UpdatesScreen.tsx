c49c5f1b6cf0455407b9f08e13868d6eb0bba944
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2020-09-12 17:48:04 -0400

    added contents

--- screens/UpdatesScreen.tsx
+++ screens/UpdatesScreen.tsx
@@ -3,11 +3,26 @@ import React from 'react'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 import { View, Dimensions, StyleSheet } from 'react-native'
 import * as Location from 'expo-location'
+import UpdateBox from '../components/UpdateBox'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        paddingTop: 20
+    }
+})
 
 class UpdatesScreen extends React.Component{
     render(){
         return (
-            <View></View>
+            <View style = {styles.container}>
+                <UpdateBox title = "Weather"/>
+                <UpdateBox title = "Traffics"/>
+                <UpdateBox title = "News"/>
+
+            </View>
         )
     }
 }
