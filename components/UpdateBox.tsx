ba283984e23bb6d6772ec44c5e2608cbca2160f5
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2020-09-12 17:47:09 -0400

    Added Message boxes for updateScreen

---
+++ components/UpdateBox.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
+import PropTypes from 'prop-types'
+
+const styles = StyleSheet.create ({
+    container: {
+        width: 340,
+        height: 160,
+        borderRadius: 10,
+        backgroundColor: 'rgba(249,249,249,0.96)',
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        flexDirection: 'row',
+    }
+})
+
+const UpdateBox = props => (
+    <View style = {styles.container}>
+        <View style = {{left: 20, top: 30, position: "absolute"}}>
+            <Text style = {{fontWeight: "bold", fontSize: 30}}> {props.title}</Text>
+            <Text> Hello World</Text>
+        </View>
+        <View style = {{right: 20, position: "absolute"}}>
+            <View style = {{backgroundColor: "black", width: 95, height: 95}}>
+
+            </View>
+            <Image source = {'../assets/images/default.jpg'}/>
+
+        </View>
+    </View>
+)
+
+export default UpdateBox
